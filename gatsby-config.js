ba09360ccd30d695346c8c8a4fe69a6ebf4aaad5
Gary Sieling <gsieling@pinnacle21.com>
2022-12-29 16:36:35 -0500

    Initial map

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,12 @@ module.exports = {
     title: `Montgomery County PA Maps`,
     siteUrl: `https://www.yourdomain.tld`
   },
-  plugins: ["gatsby-plugin-theme-ui"]
+  plugins: ["gatsby-plugin-theme-ui",
+    {
+      resolve: 'gatsby-plugin-react-leaflet',
+      options: {
+        linkStyles: true // (default: true) Enable/disable loading stylesheets via CDN
+      }
+    }
+  ]
 };
