421fccadf997cd66fece554e480f3662cce01e74
Gary Sieling <gsieling@pinnacle21.com>
2022-12-29 16:17:43 -0500

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `Montgomery County PA Maps`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: ["gatsby-plugin-theme-ui"]
+};
