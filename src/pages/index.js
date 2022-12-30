53655eeef25bbd4dd6c94c9088c553be67f0f224
Gary Sieling <gsieling@pinnacle21.com>
2022-12-30 09:53:44 -0500

    facets

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 import * as React from "react";
 import { MapContainer, TileLayer, Marker, Popup, GeoJSON } from "react-leaflet";
 import hash from 'object-hash';
+import { Checkbox, Label } from "theme-ui";
 
 const position = [40.1546, -75.2216];
 const zoom = 12;
@@ -37,6 +38,7 @@ const listItemStyles = {
   fontSize: 24,
   maxWidth: 560,
   marginBottom: 30,
+  listStyleType: "none"
 }
 
 const linkStyle = {
@@ -90,73 +92,95 @@ const links = [
       "A great place to get started if you're new to web development. Designed to guide you through setting up your first Gatsby site.",
     color: "#E95800",
   },
+]
+
+const facets = [
   {
-    text: "How to Guides",
-    url: "https://www.gatsbyjs.com/docs/how-to/",
-    description:
-      "Practical step-by-step guides to help you achieve a specific goal. Most useful when you're trying to get something done.",
-    color: "#1099A8",
+    title: 'County',
+    key: 1,
+    loaded: false,
+    source: '/static/Montgomery_County_Boundary.geojson'
   },
   {
-    text: "Reference Guides",
-    url: "https://www.gatsbyjs.com/docs/reference/",
-    description:
-      "Nitty-gritty technical descriptions of how Gatsby works. Most useful when you need detailed information about Gatsby's APIs.",
-    color: "#BC027F",
-  },
-  {
-    text: "Conceptual Guides",
-    url: "https://www.gatsbyjs.com/docs/conceptual/",
-    description:
-      "Big-picture explanations of higher-level Gatsby concepts. Most useful for building understanding of a particular topic.",
-    color: "#0D96F2",
-  },
-  {
-    text: "Build and Host",
-    url: "https://www.gatsbyjs.com/cloud",
-    badge: true,
-    description:
-      "Now you’re ready to show the world! Give your Gatsby site superpowers: Build and host on Gatsby Cloud. Get started for free!",
-    color: "#663399",
-  },
+    title: 'Municipalities',
+    key: 2,
+    loaded: false,
+    source: '/static/Montgomery_County_Municipal_Boundaries.geojson'
+  }
 ]
 
 const IndexPage = () => {
+  const [boundaries, addBoundary] = React.useState([]);
 
-  const [countyBoundaries, setCountyBoundaryJson] = React.useState(null);
-
+  console.log('rendering...');
   React.useEffect(() => {
-    fetch(
-      "/static/Montgomery_County_Boundary.geojson"
-    )
-      .then(res => res.text())
-      .then(jsonText => {
-        const countyBoundaries = JSON.parse(jsonText);
-        setCountyBoundaryJson(countyBoundaries);
-        window.countyBoundaries = countyBoundaries;
-      });
-  }, []);
+    console.log('rendering...');
 
-  const [municipalBoundaries, setMunicipalBoundaryJson] = React.useState(null);
+    facets.filter(
+      (facet) => !facet.loaded
+    ).map(
+      (facet) => {
+          fetch(facet.source)
+            .then(res => res.text())
+            .then(jsonText => {
+              const boundaryJson = JSON.parse(jsonText);
+              facet.boundaries = boundaryJson;
+              facet.loaded = true;
 
-  React.useEffect(() => {
-    fetch(
-      "/static/Montgomery_County_Municipal_Boundaries.geojson"
-    )
-      .then(res => res.text())
-      .then(jsonText => {
-        const municipalBoundaries = JSON.parse(jsonText);
-        //debugger;
-        setMunicipalBoundaryJson(municipalBoundaries);
-        window.municipalBoundaries = municipalBoundaries;
-      });
-  }, []);
+              addBoundary(
+                arr => [...arr, boundaryJson]
+              );
+            });
+        });
+      }
+    );
+
+    console.log('boundaries', boundaries);
+  const facetLayers = 
+      boundaries.map(
+        (json) => {
+          console.log('hash', hash(json))
+          return (
+            <GeoJSON
+              key={hash(json)}
+              data={json} />
+          );
+        }
+      );
 
   return (
     <main style={pageStyles}>
       <h1 style={headingStyles}>
         Map
       </h1>
+
+      <h2 style={headingStyles}>Layers</h2>
+      <ul style={listStyles}>
+        {
+          facets.map(
+            (facet) =>
+              <li key={facet.key} style={listItemStyles}>
+                <Label>
+                 <Checkbox defaultChecked={true} />
+                  {facet.title}
+                </Label>
+                <ul style={listStyles}>
+                  {
+                    facet.boundaries &&
+                    facet.boundaries.features.map(
+                      (feature) => (
+                        <Label>
+                          <Checkbox defaultChecked={true} />
+                          {feature.properties.Name}
+                        </Label>
+                      )
+                    )
+                  }
+                </ul>
+              </li>
+          )
+        }
+      </ul>
 
       <MapContainer style={{ height: '400px' }} center={position} zoom={zoom} scrollWheelZoom={true}>
         <TileLayer
@@ -168,18 +192,7 @@ const IndexPage = () => {
             A pretty CSS3 popup. <br /> Easily customizable.
           </Popup>
         </Marker>
-        { countyBoundaries &&
-          <GeoJSON
-            key={/*hash(municipalBoundaries)*/'bbb'}
-            data={countyBoundaries} />
-        }
-
-        { municipalBoundaries &&
-          <GeoJSON
-            key={/*hash(municipalBoundaries)*/'aaa'}
-            data={municipalBoundaries} />
-        }
-
+        { facetLayers }
       </MapContainer>
     </main>
   )
